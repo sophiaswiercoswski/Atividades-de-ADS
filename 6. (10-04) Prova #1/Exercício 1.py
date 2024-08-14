fbcca47c240853be422ac9bff4bb11856649378e
Sophia Swiercoswski <159056111+sophiaswiercoswski@users.noreply.github.com>
2024-08-14 17:35:48 -0300

    Delete 6. (10-04) Prova #1 directory

--- 6. (10-04) Prova #1/Exercício 1.py
+++
@@ -1,7 +0,0 @@
-base = float(input("Digite o valor da base do triângulo: "))
-altura = float(input("Digite o valor da altura do triângulo: "))
-area = (base * altura) / 2
-
-print("=================================")
-print("O valor da área do triângulo é de {:.2f}".format(area))
-print("=================================")
