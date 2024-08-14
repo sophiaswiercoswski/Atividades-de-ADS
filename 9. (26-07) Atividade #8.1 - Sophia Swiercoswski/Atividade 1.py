15941c569125a36b3030f930dc3b52726e5aaac5
Sophia Swiercoswski <159056111+sophiaswiercoswski@users.noreply.github.com>
2024-08-14 17:36:39 -0300

    Delete 9. (26-07) Atividade #8.1 - Sophia Swiercoswski directory

--- 9. (26-07) Atividade #8.1 - Sophia Swiercoswski/Atividade 1.py
+++
@@ -1,8 +0,0 @@
-# Criar função que subtraia dois números
-def subtrair(n1, n2):
-    return n1 - n2
-
-# Invocar a função de subtração três vezes
-print(abs(subtrair(3, 10)))
-print(abs(subtrair(23, 10)))
-print(abs(subtrair(32, 89)))
