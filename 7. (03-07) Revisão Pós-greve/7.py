8cda45e875c7fe2a118e327b0ce8e1333c4e003d
Sophia Swiercoswski <159056111+sophiaswiercoswski@users.noreply.github.com>
2024-08-14 17:36:23 -0300

    Delete 7. (03-07) Revisão Pós-greve directory

--- 7. (03-07) Revisão Pós-greve/7.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Digite um número: "))
-
-if n < 0:
-    print(f"{n} é um número negativo")
-elif n > 0:
-    print(f"{n} é um número positivo")
-else:
-    print(f"{n} é um número neutro")
