4325da9812b87b0ca3a9155c327da968b8a62d0f
Sophia Swiercoswski <159056111+sophiaswiercoswski@users.noreply.github.com>
2024-08-15 08:05:38 -0300

    Delete Algoritmos e programação I directory

--- Algoritmos e programação I/Aulas/Aula 5 - Estruturas de repetição em Python/Exemplo_#5.2.py
+++
@@ -1,6 +0,0 @@
-países = ["Brasil", "Alemanha", "Noruega", "Bélgica", "Chile", "França", "Inglaterra", "Marrocos", "Japão", "China"]
-
-print (países)
-print ("PAÍSES")
-for país in países:
-    print(país)
--- Algoritmos e programação I/Aulas/Aula 7 - Matrizes em Python/Exemplo_#7.4.py
+++
@@ -1,7 +0,0 @@
-# Criar uma matriz e exibir as linhas e colunas
-matriz = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]]
-
-for lista in matriz:
-    print(" ".join(map(str, lista)))
