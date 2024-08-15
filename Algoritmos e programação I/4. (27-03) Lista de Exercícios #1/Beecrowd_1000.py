0693ee6ca2810ebe867fe131e7633fb2291e6983
Sophia Swiercoswski <159056111+sophiaswiercoswski@users.noreply.github.com>
2024-08-15 08:03:57 -0300

    Delete Algoritmos e programação I/4. (27-03) Lista de Exercícios #1 directory

--- Algoritmos e programação I/4. (27-03) Lista de Exercícios #1/Beecrowd_1000.py
+++
@@ -1,9 +0,0 @@
-'''
-Beecrowd: Problema 1000
-07/03/2024
-Sophia Swiercoswski
-'''
-# Objetivo: Mostrar uma mensagem na tela
-
-# Mostrar a mensagem "Hello World!"
-print ("Hello World!")
--- Algoritmos e programação I/4. (27-03) Lista de Exercícios #1/Beecrowd_1011.py
+++
@@ -1,9 +0,0 @@
-'''
-Beecrowd: Problema 1011
-13/03/2024
-Sophia Swiercoswski
-'''
-
-raio = float(input())
-volume = (4/3) * 3.14159 * raio ** 3
-print ("VOLUME = {:.3f}". format (volume))
