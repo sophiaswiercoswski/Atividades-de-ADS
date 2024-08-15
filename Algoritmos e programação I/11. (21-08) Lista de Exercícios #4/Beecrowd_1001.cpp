200a73573f8677aa13f081fbd7e8862da5300bcf
sophiaswiercoswski <sophiaswiercoswski@gmail.com>
2024-08-15 07:59:35 -0300

    Add Exercicios

---
+++ Algoritmos e programação I/11. (21-08) Lista de Exercícios #4/Beecrowd_1001.cpp
@@ -0,0 +1,19 @@
+/*
+Beecrowd: Problema 1001
+08/08/2024
+Sophia Swiercoswski
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Ler duas variáveis inteiras
+    int A, B;
+    cin >> A >> B;
+    
+    // Somar as variáveis e exibir
+    printf("X = %d\n", A + B);
+
+    return 0;
+}
