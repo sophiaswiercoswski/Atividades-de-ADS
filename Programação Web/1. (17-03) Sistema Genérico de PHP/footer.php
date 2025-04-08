3ce1a7231cb9ceda101e90a282ed53221debe54c
Sophia Swiercoswski <159056111+sophiaswiercoswski@users.noreply.github.com>
2025-04-07 22:02:50 -0300

    Delete Programação Web/1. (17-03) Sistema Genérico de PHP directory

--- Programação Web/1. (17-03) Sistema Genérico de PHP/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="mt-5 p-2 bg-dark text-white text-center fixed-bottom">
-    <p>Sistema Genérico desenvolvido nas aulas de Programação Web do curso de TADS - <?php echo $anoAtual = date('Y')?>	&copy;</p>
-</div>
